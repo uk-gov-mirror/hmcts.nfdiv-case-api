hmcts/nfdiv-case-api

Rename petitioner to applicant

src/main/java/uk/gov/hmcts/divorce/ccd/event/solicitor/page/RespondentServiceDetails.java
@@ -9,7 +9,7 @@ import uk.gov.hmcts.divorce.ccd.model.CaseData;
 import uk.gov.hmcts.divorce.ccd.model.State;
 import uk.gov.hmcts.divorce.ccd.model.UserRole;
 
-import static uk.gov.hmcts.divorce.ccd.model.UserRole.PETITIONER_SOLICITOR;
+import static uk.gov.hmcts.divorce.ccd.model.UserRole.APPLICANT_SOLICITOR;
 
 public class RespondentServiceDetails implements CcdPageConfiguration {
 
@@ -37,9 +37,9 @@ public class RespondentServiceDetails implements CcdPageConfiguration {
                     .mandatory(Organisation::getOrganisationId)
                     .done()
                 .optional(OrganisationPolicy::getOrgPolicyCaseAssignedRole,
-                    "petitionerNameChanged=\"NeverShow\"",
-                    PETITIONER_SOLICITOR)
-                .optional(OrganisationPolicy::getOrgPolicyReference, "petitionerNameChanged=\"NeverShow\"")
+                    "applicantNameChanged=\"NeverShow\"",
+                    APPLICANT_SOLICITOR)
+                .optional(OrganisationPolicy::getOrgPolicyReference, "applicantNameChanged=\"NeverShow\"")
                 .done()
             .optional(CaseData::getDerivedRespondentHomeAddress, "respondentSolicitorRepresented=\"No\"")
             .mandatory(CaseData::getDerivedRespondentCorrespondenceAddr, "respondentSolicitorRepresented=\"No\"");

src/main/java/uk/gov/hmcts/divorce/ccd/event/solicitor/page/SolAboutTheApplicant.java
@@ -1,0 +1,38 @@
+package uk.gov.hmcts.divorce.ccd.event.solicitor.page;
+
+import uk.gov.hmcts.ccd.sdk.api.Event.EventBuilder;
+import uk.gov.hmcts.ccd.sdk.api.FieldCollection.FieldCollectionBuilder;
+import uk.gov.hmcts.divorce.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.divorce.ccd.model.CaseData;
+import uk.gov.hmcts.divorce.ccd.model.State;
+import uk.gov.hmcts.divorce.ccd.model.UserRole;
+
+public class SolAboutTheApplicant implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(final FieldCollectionBuilder<CaseData, EventBuilder<CaseData, UserRole, State>> fieldCollectionBuilder) {
+
+        fieldCollectionBuilder
+            .page("SolAboutTheApplicant")
+            .pageLabel("About the applicant")
+            .label(
+                "LabelSolAboutEditingApplication-AboutApplicant",
+                "You can make changes at the end of your application.")
+            .mandatory(CaseData::getApplicantFirstName)
+            .mandatory(CaseData::getApplicantLastName)
+            .mandatory(CaseData::getApplicantNameDifferentToMarriageCertificate)
+            .label(
+                "LabelSolAboutThePetPara-2",
+                "About the applicant",
+                "applicantNameDifferentToMarriageCertificate=\"Yes\"")
+            .mandatory(CaseData::getApplicantNameChangedHow, "applicantNameDifferentToMarriageCertificate=\"Yes\"")
+            .mandatory(CaseData::getApplicantNameChangedHowOtherDetails, "applicantNameChangedHow=\"other\"")
+            .mandatory(CaseData::getDivorceWho)
+            .mandatory(CaseData::getInferredApplicantGender)
+            .mandatory(CaseData::getMarriageIsSameSexCouple)
+            .mandatory(CaseData::getDerivedApplicantHomeAddress)
+            .optional(CaseData::getApplicantPhoneNumber)
+            .optional(CaseData::getApplicantEmail)
+            .mandatory(CaseData::getApplicantContactDetailsConfidential);
+    }
+}

uk-gov-mirror/hmcts.nfdiv-case-api

src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/RespondentServiceDetails.java
@@ -9,7 +9,7 @@ import uk.gov.hmcts.divorce.common.model.CaseData;
 import uk.gov.hmcts.divorce.common.model.State;
 import uk.gov.hmcts.divorce.common.model.UserRole;
 
-import static uk.gov.hmcts.divorce.common.model.UserRole.PETITIONER_SOLICITOR;
+import static uk.gov.hmcts.divorce.common.model.UserRole.APPLICANT_SOLICITOR;
 
 public class RespondentServiceDetails implements CcdPageConfiguration {
 
@@ -33,14 +33,14 @@ public class RespondentServiceDetails implements CcdPageConfiguration {
                 "respondentSolicitorRepresented=\"Yes\"")
             .mandatory(CaseData::getRespSolDigital, "respondentSolicitorRepresented=\"Yes\"")
             .complex(CaseData::getRespondentOrganisationPolicy, "respSolDigital=\"Yes\"")
-            .complex(OrganisationPolicy::getOrganisation)
-            .mandatory(Organisation::getOrganisationId)
-            .done()
-            .optional(OrganisationPolicy::getOrgPolicyCaseAssignedRole,
-                "petitionerNameChanged=\"NeverShow\"",
-                PETITIONER_SOLICITOR)
-            .optional(OrganisationPolicy::getOrgPolicyReference, "petitionerNameChanged=\"NeverShow\"")
-            .done()
+                .complex(OrganisationPolicy::getOrganisation)
+                    .mandatory(Organisation::getOrganisationId)
+                    .done()
+                .optional(OrganisationPolicy::getOrgPolicyCaseAssignedRole,
+                    "applicantNameChanged=\"NeverShow\"",
+                    APPLICANT_SOLICITOR)
+                .optional(OrganisationPolicy::getOrgPolicyReference, "applicantNameChanged=\"NeverShow\"")
+                .done()
             .optional(CaseData::getDerivedRespondentHomeAddress, "respondentSolicitorRepresented=\"No\"")
             .mandatory(CaseData::getDerivedRespondentCorrespondenceAddr, "respondentSolicitorRepresented=\"No\"");
     }

src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutTheApplicant.java
@@ -1,0 +1,39 @@
+package uk.gov.hmcts.divorce.solicitor.event.page;
+
+
+import uk.gov.hmcts.ccd.sdk.api.Event.EventBuilder;
+import uk.gov.hmcts.ccd.sdk.api.FieldCollection.FieldCollectionBuilder;
+import uk.gov.hmcts.divorce.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.divorce.common.model.CaseData;
+import uk.gov.hmcts.divorce.common.model.State;
+import uk.gov.hmcts.divorce.common.model.UserRole;
+
+public class SolAboutTheApplicant implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(final FieldCollectionBuilder<CaseData, EventBuilder<CaseData, UserRole, State>> fieldCollectionBuilder) {
+
+        fieldCollectionBuilder
+            .page("SolAboutTheApplicant")
+            .pageLabel("About the applicant")
+            .label(
+                "LabelSolAboutEditingApplication-AboutApplicant",
+                "You can make changes at the end of your application.")
+            .mandatory(CaseData::getApplicantFirstName)
+            .mandatory(CaseData::getApplicantLastName)
+            .mandatory(CaseData::getApplicantNameDifferentToMarriageCertificate)
+            .label(
+                "LabelSolAboutThePetPara-2",
+                "About the applicant",
+                "applicantNameDifferentToMarriageCertificate=\"Yes\"")
+            .mandatory(CaseData::getApplicantNameChangedHow, "applicantNameDifferentToMarriageCertificate=\"Yes\"")
+            .mandatory(CaseData::getApplicantNameChangedHowOtherDetails, "applicantNameChangedHow=\"other\"")
+            .mandatory(CaseData::getDivorceWho)
+            .mandatory(CaseData::getInferredApplicantGender)
+            .mandatory(CaseData::getMarriageIsSameSexCouple)
+            .mandatory(CaseData::getDerivedApplicantHomeAddress)
+            .optional(CaseData::getApplicantPhoneNumber)
+            .optional(CaseData::getApplicantEmail)
+            .mandatory(CaseData::getApplicantContactDetailsConfidential);
+    }
+}